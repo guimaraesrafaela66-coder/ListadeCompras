263d5d6c522dd288521d7af380fb6887f6937405
guimaraesrafaela66-coder <guimaraesrafaela66@gmail.com>
2025-08-15 20:59:04 -0300

    Validação de erros e implementação de funcionalidades
    Foi implementado o código de validação de erros para campos vazios ou inválidos, e os recursos de adicionar, excluir e limpar tudo.
    Evento no txbProduto para adicionar ao teclarENTER.

--- ListadeCompras/Form1.cs
+++ ListadeCompras/Form1.cs
@@ -34,7 +34,91 @@ namespace ListadeCompras
 
         private void btnAdicionar_Click(object sender, EventArgs e)
         {
+            // Verificar se txbProduto está vazio:
+            if (txbProduto.Text.Length == 0)
+            {
+                MessageBox.Show("O nome do produto não pode estar vazio!",
+                    "Erro!", MessageBoxButtons.OK, MessageBoxIcon.Error);
 
+                // Mudar a cor do fundo e a cor da letra:
+                txbProduto.BackColor = Color.Red;
+                txbProduto.ForeColor = Color.White;
+            }
+            else if (txbProduto.Text.Length < 2)
+            {
+                MessageBox.Show("O nome do produto precisa ter no mínimo duas letras!",
+                     "Erro!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                // Mudar a cor do fundo e a cor da letra:
+                txbProduto.BackColor = Color.Red;
+                txbProduto.ForeColor = Color.White;
+            }
+            else
+            {
+                // Verificar se o item está na lista:
+                if (libCompras.Items.Contains(txbProduto.Text))
+                {
+                    MessageBox.Show("Já existe {txbProduto.Text} na lista!",
+                     "Erro!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {
+                    // Adicionar o item na lista:
+                    libCompras.Items.Add(txbProduto);
+                    MessageBox.Show($"{txbProduto.Text} foi adicionado na listade compras!",
+                        "Show!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                    // Retornar o txb.Produto à cor normal:
+                    txbProduto.BackColor = Color.White;
+                    txbProduto.ForeColor = Color.Black;
+
+                    // Limpar o campo:
+                    txbProduto.Text = "";
+                }
+            }
+
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            // Perguntar se4 realmente quer apagar:
+            DialogResult resposta = MessageBox.Show("Tem certeza que deseja apagar tudo?",
+                "Atenção", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            // Se optar por "sim" , apagar:
+            if (resposta == DialogResult.Yes)
+            {
+                libCompras.Items.Clear();
+            }
+        }
+
+        private void btnExcluir_Click(object sender, EventArgs e)
+        {
+            // Verificar se o usuário não selecionou nada:
+            if (libCompras.SelectedIndex == -1)
+            {
+                MessageBox.Show("Selecione um item para remover!",
+                    "Erroo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+
+            {
+                // Salvar temporariamente o nome de item que será removido:
+                string itemRemovido = libCompras.SelectedItem.ToString();
+
+                // Rempver o item selecionado:
+                libCompras.Items.RemoveAt(libCompras.SelectedIndex);
+
+                MessageBox.Show($"O item foi removido da lista",
+                    "Show!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }   }
+
+        private void txbProduto_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                // "Pressionar" 0o btnAdicionar:
+                btnAdicionar.PerformClick();
+            }
         }
     }
 }
